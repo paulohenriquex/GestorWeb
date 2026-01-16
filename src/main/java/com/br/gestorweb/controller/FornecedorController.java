bc002b84ee39bb007e5ab98ad25e805e652b43aa
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-16 08:55:37 -0300

    Criação da classe fornecedorController e dos métodos cadastrarFornecedor e listarFornecedores.

---
+++ src/main/java/com/br/gestorweb/controller/FornecedorController.java
@@ -0,0 +1,31 @@
+package com.br.gestorweb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.gestorweb.model.Fornecedor;
+import com.br.gestorweb.service.FornecedorService;
+
+@RestController
+@RequestMapping("/fornecedor")
+public class FornecedorController {
+    @Autowired
+    public FornecedorService fornecedorService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Fornecedor> cadastrarFornecedor(@RequestBody Fornecedor fornecedor) {
+        return fornecedorService.save(fornecedor);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Fornecedor>> listarFornecedores() {
+        return fornecedorService.findAll();
+    }
+}
