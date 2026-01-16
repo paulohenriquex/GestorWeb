d3ac33543ec6dd6b72f886f963b97dae94fbbb73
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-16 08:55:05 -0300

    Criação da classe categoriaController e dos métodos cadastrarCategoria e listarCategoria.

---
+++ src/main/java/com/br/gestorweb/controller/CategoriaController.java
@@ -0,0 +1,32 @@
+package com.br.gestorweb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.gestorweb.model.Categoria;
+import com.br.gestorweb.service.CategoriaService;
+
+@RestController
+@RequestMapping("/categoria")
+public class CategoriaController {
+
+    @Autowired
+    public CategoriaService categoriaService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Categoria> cadastrarCategoria(@RequestBody Categoria categoria) {
+        return categoriaService.save(categoria);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Categoria>> listarCategorias() {
+        return categoriaService.findAll();
+    }
+}
