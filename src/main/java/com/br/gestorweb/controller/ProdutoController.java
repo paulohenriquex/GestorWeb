6a09b81c70649a4c24e5d941be0df7b627ef8f00
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-16 08:56:21 -0300

    Criação da classe produtoController e dos métodos cadastrarProduto e listarProduto.

---
+++ src/main/java/com/br/gestorweb/controller/ProdutoController.java
@@ -0,0 +1,32 @@
+package com.br.gestorweb.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.br.gestorweb.model.Produto;
+import com.br.gestorweb.service.ProdutoService;
+
+@RestController
+@RequestMapping("/produto")
+public class ProdutoController {
+
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping("/cadastrar")
+    public ResponseEntity<Produto> salvar(@RequestBody Produto produto) {
+        return produtoService.save(produto);
+    }
+
+    @GetMapping("/listar")
+    public ResponseEntity<List<Produto>> listar() {
+        return produtoService.findAll();
+    }
+}
