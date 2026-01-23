66b070087dec37e45d679b524c346f187af72346
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-23 18:38:43 -0300

    Adição do CrossOrigin e modificação do GetMapping, e adição do DeleteMapping.

--- src/main/java/com/br/gestorweb/controller/ProdutoController.java
+++ src/main/java/com/br/gestorweb/controller/ProdutoController.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +17,8 @@ import com.br.gestorweb.model.Produto;
 import com.br.gestorweb.service.ProdutoService;
 
 @RestController
-@RequestMapping("/produto")
+@RequestMapping("/produtos")
+@CrossOrigin(origins = "http://localhost:5173")
 public class ProdutoController {
 
     @Autowired
@@ -28,6 +32,20 @@ public class ProdutoController {
 
     @GetMapping("/listar")
     public ResponseEntity<List<Produto>> listar() {
-        return produtoService.findAll();
+        List<Produto> produtos = produtoService.findAll();
+        if (produtos == null) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(produtos);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProduto(@PathVariable Long id) {
+        try {
+            produtoService.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().build();
+        }
     }
 }
