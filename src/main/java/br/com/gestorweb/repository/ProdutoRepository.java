5b52acf1dc8e8457948cd4c28bac2fa79cc3bab5
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-29 08:30:07 -0300

    Criação das classes Repository.

---
+++ src/main/java/br/com/gestorweb/repository/ProdutoRepository.java
@@ -0,0 +1,12 @@
+package br.com.gestorweb.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.gestorweb.model.Produto;
+
+public interface ProdutoRepository extends JpaRepository<Produto, Long> {
+
+    List<Produto> All();
+}
