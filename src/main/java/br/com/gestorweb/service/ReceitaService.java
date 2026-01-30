d565c0392d0f2ed6e63958c46cd0371e5fa754fb
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-30 08:31:05 -0300

    Modificação na classe para adicionar corretamente os ingredientes, e verificação de nullexeption.

--- src/main/java/br/com/gestorweb/service/ReceitaService.java
+++ src/main/java/br/com/gestorweb/service/ReceitaService.java
@@ -1,29 +1,70 @@
 package br.com.gestorweb.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
+import br.com.gestorweb.dto.IngredienteReceitaDTO;
 import br.com.gestorweb.dto.ReceitaDTO;
+import br.com.gestorweb.model.IngredienteReceita;
 import br.com.gestorweb.model.Receita;
+import br.com.gestorweb.repository.ProdutoRepository;
 import br.com.gestorweb.repository.ReceitaRepository;
 
 @Service
 public class ReceitaService {
     private final ReceitaRepository receitaRepository;
+    private final ProdutoRepository produtoRepository;
 
-    public ReceitaService(ReceitaRepository receitaRepository) {
+    public ReceitaService(ReceitaRepository receitaRepository, ProdutoRepository produtoRepository) {
         this.receitaRepository = receitaRepository;
+        this.produtoRepository = produtoRepository;
     }
 
     public ReceitaDTO save(ReceitaDTO dto) {
         Receita receita = new Receita();
+        receita.setId(dto.id());
         receita.setNome(dto.nome());
         receita.setModoDePreparo(dto.modoDePreparo());
+
+        if (dto.ingredientes() != null) {
+            List<IngredienteReceita> listaDeIngredientes = new ArrayList<>();
+            for (IngredienteReceitaDTO ingredienteDTO : dto.ingredientes()) {
+                IngredienteReceita ingrediente = new IngredienteReceita();
+                ingrediente.setPercapita(ingredienteDTO.percapita());
+                if (ingredienteDTO.produtoId() != null) {
+                    ingrediente.setProduto(produtoRepository.getReferenceById(ingredienteDTO.produtoId()));
+                }
+
+                ingrediente.setReceita(receita);
+                listaDeIngredientes.add(ingrediente);
+            }
+            receita.setIngredientes(listaDeIngredientes);
+        }
         receita = receitaRepository.save(receita);
         return converterParaDTO(receita);
     }
 
     private ReceitaDTO converterParaDTO(Receita receita) {
-        return new ReceitaDTO(receita.getId(), receita.getNome(), receita.getModoDePreparo());
+        List<IngredienteReceitaDTO> ingredientesDTO = new ArrayList<>();
+        if (receita.getIngredientes() != null) {
+            for (IngredienteReceita ingrediente : receita.getIngredientes()) {
+                Long produtoId = (ingrediente.getProduto() != null) ? ingrediente.getProduto().getId() : null;
+
+                ingredientesDTO.add(new IngredienteReceitaDTO(
+                        ingrediente.getId(),
+                        produtoId,
+                        receita.getId(),
+                        ingrediente.getPercapita()));
+            }
+
+        }
+        return new ReceitaDTO(
+                receita.getId(),
+                receita.getNome(),
+                receita.getModoDePreparo(),
+                ingredientesDTO);
     }
 
 }
