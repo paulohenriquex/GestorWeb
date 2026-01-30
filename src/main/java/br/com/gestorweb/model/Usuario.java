a85660bf14df88b90420b652a5bbcb7176b9b31e
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-30 07:48:26 -0300

    Criação da classe Usuario.

--- src/main/java/br/com/gestorweb/model/Usuario.java
+++ src/main/java/br/com/gestorweb/model/Usuario.java
@@ -18,4 +18,8 @@ public class Usuario {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String email;
+    private String senha;
+    private String role;
 }
