5d8c4173fc55598e891798e027c1777345e3df61
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-30 08:59:13 -0300

    Criação das colunas planejamento, usuario.

--- src/main/java/br/com/gestorweb/model/Servico.java
+++ src/main/java/br/com/gestorweb/model/Servico.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -20,4 +22,12 @@ public class Servico {
     private Long id;
 
     private String nome;
+
+    @ManyToOne
+    @JoinColumn(name = "planejamento_id")
+    private Planejamento planejamento;
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
 }
