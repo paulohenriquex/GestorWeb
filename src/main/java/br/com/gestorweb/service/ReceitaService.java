10ce974b8d953fda47ca95ca0e62f93690376b18
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-30 07:50:31 -0300

    Criação da classe ReceitaService.

---
+++ src/main/java/br/com/gestorweb/service/ReceitaService.java
@@ -0,0 +1,29 @@
+package br.com.gestorweb.service;
+
+import org.springframework.stereotype.Service;
+
+import br.com.gestorweb.dto.ReceitaDTO;
+import br.com.gestorweb.model.Receita;
+import br.com.gestorweb.repository.ReceitaRepository;
+
+@Service
+public class ReceitaService {
+    private final ReceitaRepository receitaRepository;
+
+    public ReceitaService(ReceitaRepository receitaRepository) {
+        this.receitaRepository = receitaRepository;
+    }
+
+    public ReceitaDTO save(ReceitaDTO dto) {
+        Receita receita = new Receita();
+        receita.setNome(dto.nome());
+        receita.setModoDePreparo(dto.modoDePreparo());
+        receita = receitaRepository.save(receita);
+        return converterParaDTO(receita);
+    }
+
+    public ReceitaDTO converterParaDTO(Receita receita) {
+        return new ReceitaDTO(receita.getId(), receita.getNome(), receita.getModoDePreparo());
+    }
+
+}
