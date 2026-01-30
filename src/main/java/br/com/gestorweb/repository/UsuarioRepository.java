b4c67c97185a1f919e52cb395cdf3878292290f7
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-30 09:19:29 -0300

    Criado UsuarioRepository.

--- src/main/java/br/com/gestorweb/repository/UsuarioRepository.java
+++ src/main/java/br/com/gestorweb/repository/UsuarioRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.gestorweb.model.Usuario;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
+    
 }
