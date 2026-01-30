7124c60f67e173bced823c68c5be99edd3aa4ec5
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-30 08:04:37 -0300

    Modificação nos argumentos da classe.

--- src/main/java/br/com/gestorweb/dto/EnderecoDTO.java
+++ src/main/java/br/com/gestorweb/dto/EnderecoDTO.java
@@ -1,6 +1,6 @@
 package br.com.gestorweb.dto;
 
-public record EnderecoDTO(Long id, String logradouro, String pais, String estado, String cidade, String bairro,
-        String cep, String numero, String complemento) {
+public record EnderecoDTO(Long id, String pais, String estado, String cidade, String bairro,
+                String logradouro, String cep, String numero, String complemento) {
 
 }
