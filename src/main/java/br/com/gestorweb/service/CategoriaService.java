81a84ef2f7c850592ee594456d6a2446aa70f144
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-30 07:49:11 -0300

    Criação do findAll, e @Transicional.

--- src/main/java/br/com/gestorweb/service/CategoriaService.java
+++ src/main/java/br/com/gestorweb/service/CategoriaService.java
@@ -1,10 +1,14 @@
 package br.com.gestorweb.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import br.com.gestorweb.dto.CategoriaDTO;
 import br.com.gestorweb.model.Categoria;
 import br.com.gestorweb.repository.CategoriaRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class CategoriaService {
@@ -14,11 +18,21 @@ public class CategoriaService {
         this.categoriaRepository = categoriaRepository;
     }
 
+    @Transactional
     public CategoriaDTO save(CategoriaDTO dto) {
         Categoria categoria = new Categoria();
         categoria.setNome(dto.nome());
         categoria = categoriaRepository.save(categoria);
         return converterParaDTO(categoria);
+    }
+
+    public List<CategoriaDTO> findAll() {
+        List<Categoria> categoriasNoBanco = categoriaRepository.findAll();
+        List<CategoriaDTO> listaDeDtos = new ArrayList<>();
+        for (Categoria categoria : categoriasNoBanco) {
+            listaDeDtos.add(converterParaDTO(categoria));
+        }
+        return listaDeDtos;
     }
 
     public CategoriaDTO converterParaDTO(Categoria categoria) {
