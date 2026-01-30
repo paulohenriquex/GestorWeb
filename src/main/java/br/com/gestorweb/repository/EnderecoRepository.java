9066d1854c479ab7cbf88fe9d456eae8854068ce
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-30 07:23:47 -0300

    Criação do EnderecoRepository e FornecedorRepository.

--- src/main/java/br/com/gestorweb/repository/EnderecoRepository.java
+++ src/main/java/br/com/gestorweb/repository/EnderecoRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gestorweb.model.Endereco;
 
-public interface EnderecoRepository extends JpaRepository<Long, Endereco> {
+public interface EnderecoRepository extends JpaRepository<Endereco, Long> {
 
 }
--- src/main/java/br/com/gestorweb/repository/FornecedorRepository.java
+++ src/main/java/br/com/gestorweb/repository/FornecedorRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.gestorweb.model.Fornecedor;
 
-public interface FornecedorRepository extends JpaRepository<Long, Fornecedor> {
+public interface FornecedorRepository extends JpaRepository<Fornecedor, Long> {
 
 }
