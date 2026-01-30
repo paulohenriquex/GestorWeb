f043685a595714d2acdab802fa215ac39002ca8d
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-30 09:00:10 -0300

    Criando as colunas (produtos, receitas, fornecedores, servicos, planejamentos, marcas, categorias, enderecos, ingredientes)

--- src/main/java/br/com/gestorweb/model/Usuario.java
+++ src/main/java/br/com/gestorweb/model/Usuario.java
@@ -1,9 +1,12 @@
 package br.com.gestorweb.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -22,4 +25,32 @@ public class Usuario {
     private String email;
     private String senha;
     private String role;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Produto> produtos;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Receita> receitas;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Fornecedor> fornecedores;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Servico> servicos;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Planejamento> planejamentos;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Marca> marcas;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Categoria> categorias;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Endereco> enderecos;
+
+    @OneToMany(mappedBy = "usuario")
+    private List<IngredienteReceita> ingredientes;
+
 }
