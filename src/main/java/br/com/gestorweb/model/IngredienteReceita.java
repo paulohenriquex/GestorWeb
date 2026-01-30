47940ae249307fa347375d155e74a6198c059a3e
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-30 07:47:11 -0300

    Adição ManyToOne e JoinColumn para receita e produto.

--- src/main/java/br/com/gestorweb/model/IngredienteReceita.java
+++ src/main/java/br/com/gestorweb/model/IngredienteReceita.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -19,6 +21,13 @@ public class IngredienteReceita {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "receita_id")
     private Receita receita;
+
+    @ManyToOne
+    @JoinColumn(name = "produto_id")
     private Produto produto;
+
+    private Double percapita;
 }
