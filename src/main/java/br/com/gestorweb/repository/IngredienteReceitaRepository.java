b5a45a3929e2f9c2a6df33c8cb2def64f60eca6b
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-31 09:44:14 -0300

    feat(repository): cria interfaces básicas para IngredienteReceita e Planejamento
    - Implementa herança de JpaRepository para operações padrão de CRUD.
    - Habilita o gerenciamento de persistência para as entidades de cálculo e agenda.

---
+++ src/main/java/br/com/gestorweb/repository/IngredienteReceitaRepository.java
@@ -0,0 +1,9 @@
+package br.com.gestorweb.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.com.gestorweb.model.IngredienteReceita;
+
+public interface IngredienteReceitaRepository extends JpaRepository<IngredienteReceita, Long> {
+
+}
