e95ea7c10a3923df15383b111001906ccd054268
paulohenriquex <paulohenriquesouzabr@outlook.com>
2026-01-31 09:43:14 -0300

    feat(model): implementa lógica de associação em ServicoPlanejamento
    Define a entidade intermediária entre Planejamento e Servico, adicionando
    suporte a listas de receitas associadas e controle de quantidade de comensais
    por tipo de serviço.

--- src/main/java/br/com/gestorweb/model/ServicoPlanejamento.java
+++ src/main/java/br/com/gestorweb/model/ServicoPlanejamento.java
@@ -1,11 +1,14 @@
 package br.com.gestorweb.model;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -21,11 +24,20 @@ public class ServicoPlanejamento {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "planejamento_id")
     private Planejamento planejamento;
 
+    @ManyToOne
+    @JoinColumn(name = "servico_id")
     private Servico servico;
 
     @ManyToOne
     @JoinColumn(name = "usuario_id")
     private Usuario usuario;
+
+    private Integer quantitativo; // Number of people for this specific service in this planning
+
+    @OneToMany(mappedBy = "servicoPlanejamento", cascade = jakarta.persistence.CascadeType.ALL, orphanRemoval = true)
+    private List<ServicoReceita> servicoReceitas;
 }
